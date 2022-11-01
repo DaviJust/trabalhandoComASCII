357fa0d87885ed6d145c06facedcdaa69acb4948
Davi <davijustinocouto@gmail.com>
2022-10-31 23:08:02 -0300

    Tentativa numero 2,3

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-const button = document.getElementById(button)
-const input = document.getElementById(input)
-button.onclick = function() {
-    let texto = input.charCodeAt(0)
-    input.innerHTML = texto
-}
+//Meu codigo
+// const button = document.getElementById("button")
+// const input = document.getElementById("input").value;
+// button.onclick = function() {
+//     var input2 = document.getElementById("input").value;
+//     var texto = input2.charCodeAt()
+//     document.getElementById("demo").innerHTML = texto;
+// }
 
+// Codigo no stackoverflow https://stackoverflow.com/questions/94037/convert-character-to-ascii-code-in-javascript
+// function myFunction(){
+//     var str=document.getElementById("id1");
+//     if (str.value=="") {
+//        str.focus();
+//        return;
+//     }
+//     var a="ASCII Code is == >  ";
+// document.getElementById("demo").innerHTML =a+str.value.charCodeAt(0);
+// }
