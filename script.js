8212e659d2724f8fbde8b2a98ce0ade284ea7f87
Davi <davijustinocouto@gmail.com>
2022-11-01 20:15:07 -0300

    Exercicio ASCII feito

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-
-const button = document.getElementById("button")
-const input = document.getElementById("input").value;
-button.onclick = function() {
-    var texto = input2.charCodeAt().value
-    input.innerHTML = texto;
-}
+// const button = document.getElementById("button")
+// button.onclick = function() {
+// const input = document.getElementById("input").value;
+//     var texto = input2.charCodeAt().value
+//     input.innerHTML = texto;
+// }
 
 // Codigo no stackoverflow https://stackoverflow.com/questions/94037/convert-character-to-ascii-code-in-javascript
-// function myFunction(){
-//     var str=document.getElementById("id1");
-//     if (str.value=="") {
-//        str.focus();
-//        return;
-//     }
-//     var a="ASCII Code is == >  ";
-// document.getElementById("demo").innerHTML =a+str.value.charCodeAt(0);
-// }
+
+const button = document.getElementById("button")
+button.onclick = function() {
+    alert("ASCII")
+    var str = document.getElementById("input");
+    if (str.value=="") {
+       str.focus();
+       return;
+       
+    }
+document.getElementById("exampleFormControlTextarea1").innerText = str.value.charCodeAt(0);
+}
+
+//Nao havia funcionado antes por causa do innerhtml, era para ter sido innertext e tambem ao em vez do output ser a tag input, precisa ser <textarea>
